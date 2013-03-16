1c169a59eea9b7f4530105b4c77f319ad25eb6e6
rameshreddypv <rameshreddy717@hotmail.com>
2013-03-16 20:14:01 +0530

    Update New.java

--- JavaProject/New.java
+++ JavaProject/New.java
@@ -7,5 +7,8 @@ System.out.println("New class Constructor");
 public void add(){
 System.out.prinln("this is add method");
 }
+public void division{
+System.out.prinln("this is division method");
+}
 
 }
